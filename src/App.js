19ce0ec72f9c99a804fed81e285a81056713a5c3
showeimer <showeimer@gmail.com>
2017-09-11 11:51:43 -0400

    reinstalled dependencies due to some unknown error after installing firebase, created LoginForm, imported into App, created first textinput field

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
-import {Header, Card, CardSection, Button} from './components/common';
+import firebase from 'firebase';
+import {Header} from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+componentDidMount() {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAqfV5LyLmtDpkDvg_G5Vuqw05GJghi5Ss',
+    authDomain: 'auth-fee66.firebaseapp.com',
+    databaseURL: 'https://auth-fee66.firebaseio.com',
+    projectId: 'auth-fee66',
+    storageBucket: 'auth-fee66.appspot.com',
+    messagingSenderId: '708212966805'
+  });
+}
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     )
   }
