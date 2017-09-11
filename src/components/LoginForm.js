fb5b61b4a0c41bff12f8e77dcc2493770f09533a
showeimer <showeimer@gmail.com>
2017-09-11 13:22:04 -0400

    created Input component, imported into index.js for easy access

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
-import {View, TextInput} from 'react-native';
+import {TextInput} from 'react-native';
 import {Button, Card, CardSection} from './common';
 
 class LoginForm extends Component {
+  state = { text: ''}
+
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput style={{height: 20, width: 100}}/>
+          <TextInput
+            value={this.state.text}
+            onChangetext={text => this.setState({text:event.target.value})}
+            style={{height: 20, width: 100}}/>
         </CardSection>
 
         <CardSection>
