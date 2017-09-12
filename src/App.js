04d9fb61670856a37c0c9cf360b99daa6e298611
showeimer <showeimer@gmail.com>
2017-09-12 11:34:34 -0400

    added handling for successful login, added some code to improve UX to render Spinner when app boots up and figures out if the user is logged in or not, successful log in simply renders a functional logout button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import firebase from 'firebase';
-import {Header} from './components/common';
+import {Header, Button, Spinner, CardSection} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {loggedIn: null};
 
-componentDidMount() {
-  firebase.initializeApp({
-    apiKey: 'AIzaSyAqfV5LyLmtDpkDvg_G5Vuqw05GJghi5Ss',
-    authDomain: 'auth-fee66.firebaseapp.com',
-    databaseURL: 'https://auth-fee66.firebaseio.com',
-    projectId: 'auth-fee66',
-    storageBucket: 'auth-fee66.appspot.com',
-    messagingSenderId: '708212966805'
-  });
-}
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAqfV5LyLmtDpkDvg_G5Vuqw05GJghi5Ss',
+      authDomain: 'auth-fee66.firebaseapp.com',
+      databaseURL: 'https://auth-fee66.firebaseio.com',
+      projectId: 'auth-fee66',
+      storageBucket: 'auth-fee66.appspot.com',
+      messagingSenderId: '708212966805'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({loggedIn: true})
+      } else {
+        this.setState({loggedIn: false})
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Logout
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
+  }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     )
   }
